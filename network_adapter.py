162ae847ba6c0a1a53985730bfc56ee148af8e25
dperpel <80916191+dperpel@users.noreply.github.com>
2021-04-08 17:07:34 +0300

    Add files via upload

--- network_adapter.py
+++ network_adapter.py
@@ -1,14 +1,18 @@
 class Adapter() : 
     
     #Constructor
-    def __init__(self, MAC) :
+    def __init__(self, MAC, IP, Mask, Gateway, DNS) :
         self.MAC = MAC
-    
+        self.IP = IP
+        self.Mask = Mask
+        self.Gateway = Gateway
+        self.DNS = DNS
+
     #Adapter Object Equalizer
     def __eq__(self, other): 
         if not isinstance(other, Adapter):
             #Do not attempt to compare against unrelated types
             return NotImplemented
         
-        #compare the rest of the attributes - TODO 
+        #compare the rest of the attributes if needed - TODO 
         return self.MAC == other.MAC
